0bf8639b0bb511e69b12121b96284f2ec10ed17b
OCHHQ <enosejecollins@gmail.com>
2024-10-20 04:40:52 +0100

    update

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,12 @@
-DELIMITER $$
+DELIMITER //
 
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    -- Check if the email is being changed
-    IF NEW.email <> OLD.email THEN
-        -- Reset valid_email to 0
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
-END$$
+END //
 
 DELIMITER ;
